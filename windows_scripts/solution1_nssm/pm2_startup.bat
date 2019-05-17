d8ea55f73da7315d48246e41e82662ca3c23e64e
Moshe Quantz <moshequ@gmail.com>
2019-05-17 20:24:16 +0300

    Update pm2_startup.bat

--- windows_scripts/solution1_nssm/pm2_startup.bat
+++ windows_scripts/solution1_nssm/pm2_startup.bat
@@ -7,10 +7,7 @@ set PM2_HOME=c:\etc\.pm2
 set path=C:\Users\walter\AppData\Roaming\npm;%path%
 
 @REM Optionally, you can add 'pm2 kill' just before 
-@REM  resurrect (adding a sleep between 2 commands):
-@REM  	pm2 kill
-@REM  	timeout /t 5 /nobreak > NUL
-@REM  	pm2 resurrect
+@REM  	pm2 kill & pm2 resurrect
 @REM otherwise, you can simple call resurrect as follow:
 pm2 resurrect
 
